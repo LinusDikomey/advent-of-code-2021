7bb52ea56bf76540341be721462264938b9a25a6
LinusDikomey <l.dikomey03@gmail.com>
2021-12-02 13:39:28 +0100

    day 2 nicer solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -34,5 +34,58 @@ fn main() {
             _ => panic!()
         }
     }
-    println!("Part 1: {}", pos * depth);
+    println!("Part 2: {}", pos * depth);
+
+    nicer_solution();
+}
+
+fn nicer_solution() {
+    enum Command {
+        Forward(i32),
+        Up(i32),
+        Down(i32)
+    }
+    use Command::*;
+    impl From<&str> for Command {
+        fn from(s: &str) -> Self {
+            let mut segments = s.split(' ');
+            let command = segments.next().unwrap();
+            let num = segments.next().unwrap().parse::<i32>().unwrap();
+            match command {
+                "forward" => Forward(num),
+                "up" => Up(num),
+                "down" => Down(num),
+                _ => panic!()
+            }
+        }
+    }
+    let commands: Vec<Command> = include_str!("input.txt").lines().map(Command::from).collect();
+
+    // Part 1
+    let mut depth = 0;
+    let mut pos = 0;
+    for command in &commands {
+        match command {
+            Up(x) => depth -= x,
+            Down(x) => depth += x,
+            Forward(x) => pos += x
+        }
+    }
+    println!("Part 1: {}", depth * pos);
+
+    // Part 2
+    let mut depth = 0;
+    let mut pos = 0;
+    let mut aim = 0;
+    for command in &commands {
+        match command {
+            Up(x) => aim -= x,
+            Down(x) => aim += x,
+            Forward(x) => {
+                pos += x;
+                depth += x * aim;
+            }
+        }
+    }
+    println!("Part 2: {}", depth * pos);
 }
