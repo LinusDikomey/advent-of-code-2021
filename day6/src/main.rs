db5b7572ec88e8f79afd735bbea7a8dad0a930c8
LinusDikomey <l.dikomey03@gmail.com>
2021-12-06 09:26:24 +0100

    day 6

---
+++ day6/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let fishes = include_str!("input.txt").
+        split(',').map(|s| s.trim().parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+    println!("Part 1: {}", simulate(fishes.clone(), 80));
+    println!("Part 2: {}", simulate_optimized(&fishes, 256));
+}
+
+fn simulate(mut fishes: Vec<u32>, days: usize) -> usize {
+    for _ in 0..days {
+        let mut new = 0;
+        for fish in &mut fishes {
+            if *fish == 0 {
+                new += 1;
+                *fish = 6;
+            } else {
+                *fish -= 1;
+            }
+        }
+        fishes.extend((0..new).map(|_| 8));
+    }
+    fishes.len()
+}
+
+fn simulate_optimized(fishes: &[u32], days: usize) -> u64 {
+    let mut fishes_by_day = [0_u64; 9];
+    for fish in fishes {
+        fishes_by_day[*fish as usize] += 1;
+    }
+    for _ in 0..days {
+        fishes_by_day.rotate_left(1);
+        fishes_by_day[6] += fishes_by_day[8];
+    }
+    fishes_by_day.iter().sum()
+}
