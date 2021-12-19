21a8f232f00ca2cc6d881a3e7588e4ba2d0d5fe2
LinusDikomey <l.dikomey03@gmail.com>
2021-12-20 00:15:25 +0100

    day 18

--- src/day17.rs
+++ src/day17.rs
@@ -1,9 +1,49 @@
-pub fn run(_input: &str) {
-    println!("Day 17:");
-    // ---------- part 1 ----------
-    
-    println!("\tPart 1: {}", 0);
-    // ---------- part 2 ----------
+use std::ops::RangeInclusive;
 
-    println!("\tPart 2: {}", 0);
+pub fn run(_input: &str) {
+    let target = (169..=206, -108..=-68);
+    println!("Day 17:");
+    let (highest, hit_count) = find_highest(target);
+    // ---------- part 1 ----------
+    println!("\tPart 1: {}", highest);
+    // ---------- part 2 ----------
+    println!("\tPart 2: {}", hit_count);
+}
+
+fn find_highest(target: (RangeInclusive<i32>, RangeInclusive<i32>)) -> (i32, i32) {
+    let mut highest = 0;
+    let mut hit_count = 0;
+    'y_loop: for y_vel in *target.1.start()..1000 {
+        //println!("Trying y {}, highest: {}", y_vel, highest);
+        'x_loop: for x_vel in 0.. {
+            //println!("Trying x {}", x_vel);
+            let mut pos = (0, 0);
+            let mut vel = (x_vel, y_vel);
+            let mut max_y = 0;
+            for step in 0.. {
+                max_y = std::cmp::max(max_y, pos.1);
+                //println!("\tStep {}: pos: {:?}, vel: {:?}", step, pos, vel);
+                if target.0.contains(&pos.0) && target.1.contains(&pos.1) {
+                    highest = max_y;
+                    hit_count += 1;
+                    continue 'x_loop;
+                }
+
+                if vel.0 == 0 && !target.0.contains(&pos.0) || pos.1 < *target.1.start() {
+                    continue 'x_loop;
+                }
+                simulate_step(&mut pos, &mut vel);
+                if step == 0 && pos.0 > *target.0.end() { continue 'y_loop; }
+            }
+        }
+    }
+    (highest, hit_count)
+}
+
+fn simulate_step(pos: &mut (i32, i32), vel: &mut (i32, i32)) {
+    pos.0 += vel.0;
+    pos.1 += vel.1;
+
+    vel.0 -= vel.0.signum();
+    vel.1 -= 1;
 }
--- src/day18.rs
+++ src/day18.rs
@@ -1,9 +1,124 @@
-pub fn run(_input: &str) {
+#[derive(Clone)]
+enum SnailNumber {
+    Number(u8),
+    Pair(Box<SnailNumber>, Box<SnailNumber>)
+}
+use std::str::Chars;
+
+use SnailNumber::*;
+impl std::fmt::Display for SnailNumber {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Number(n) => write!(f, "{}", n),
+            Pair(l, r) => write!(f, "[{}, {}]", l, r)
+        }
+        
+    }
+}
+impl SnailNumber {
+    fn parse(s: &mut Chars) -> Self {
+        match s.next().unwrap() {
+            '[' => {
+                let l = Box::new(Self::parse(s));
+                let comma = s.next().unwrap();
+                assert_eq!(comma, ',');
+                let r = Box::new(Self::parse(s));
+                let p = Pair(l, r);
+                let closing = s.next().unwrap();
+                assert_eq!(closing, ']');
+                p
+            },
+            c => Number(c.to_string().parse().unwrap())
+        }
+    }
+    fn unwrap_number(&mut self) -> u8 {
+        match self {
+            Number(n) => *n,
+            Pair(_, _) => panic!("Tried to unwrap pair as number!")
+        }
+    }
+    fn add_l(&mut self, x: u8) {
+        match self {
+            Number(n) => *n += x,
+            Pair(l, _) => l.add_l(x)
+        }
+    }
+    fn add_r(&mut self, x: u8) {
+        match self {
+            Number(n) => *n += x,
+            Pair(_, r) => r.add_r(x)
+        }
+    }
+    fn try_explode(&mut self, depth: usize) -> Option<(Option<u8>, Option<u8>)> {
+        match self {
+            Number(_) => None,
+            Pair(l, r) => if depth == 4 {
+                let res = Some((Some(l.unwrap_number()), Some(r.unwrap_number())));
+                *self = Number(0);
+                res
+            } else {
+                if let Some((exploded_l, mut exploded_r)) = l.try_explode(depth + 1) {
+                    if let Some(exploded_r) = exploded_r.take() {
+                        r.add_l(exploded_r);
+                    }
+                    Some((exploded_l, exploded_r))
+                } else {
+                    if let Some((mut exploded_l, exploded_r)) = r.try_explode(depth + 1) {
+                        if let Some(exploded_l) = exploded_l.take() {
+                            l.add_r(exploded_l);
+                        }
+                        Some((exploded_l, exploded_r))
+                    } else {
+                        None
+                    }
+                }
+            }
+        }
+    }
+    fn try_split(&mut self) -> bool {
+        match self {
+            Number(n) => if *n >= 10 {
+                *self = Pair(Box::new(Number(*n/2)), Box::new(Number(*n/2 + if *n % 2 == 1 {1} else {0})));
+                true
+            } else {
+                false
+            },
+            Pair(l, r) => l.try_split() || r.try_split()
+        }
+    }
+    fn reduce(&mut self) {
+        loop {
+            if self.try_explode(0).is_some() { continue; }
+            if self.try_split() { continue; }
+            break;
+        }
+    }
+    fn magnitude(&self) -> u64 {
+        match self {
+            Number(n) => *n as u64,
+            Pair(l, r) => 3 * l.magnitude() + 2 * r.magnitude()
+        }
+    }
+}
+
+fn add(l: SnailNumber, r: SnailNumber) -> SnailNumber {
+    let mut new = SnailNumber::Pair(Box::new(l), Box::new(r));
+    new.reduce();
+    new
+}
+
+pub fn run(input: &str) {
+    let input = input.lines().map(|line| SnailNumber::parse(&mut line.chars())).collect::<Vec<_>>();
     println!("Day 18:");
     // ---------- part 1 ----------
-    
-    println!("\tPart 1: {}", 0);
+    println!("\tPart 1: {}", input.iter().cloned().reduce(|l, r| add(l, r)).unwrap().magnitude());
     // ---------- part 2 ----------
-
-    println!("\tPart 2: {}", 0);
+    println!("\tPart 2: {}", input.iter().cloned().enumerate()
+        .map(|(i, l)| input.iter().cloned().enumerate()
+            .filter(move |(i2, _)| i != *i2)
+            .map(move |(_, r)| add(l.clone(), r).magnitude())
+        )
+        .flatten()
+        .max().unwrap()
+    );
 }
